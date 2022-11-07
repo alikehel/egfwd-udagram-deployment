f21661ff67e2fdd9578ec40ee42750a7b1fd1fc9
Ali Kehel <ali.saber.27.2@gmail.com>
2022-11-07 14:08:59 +0200

    fix error

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,4 +1,1 @@
-eb init udagram-api --platform node.js --region us-east-1 &&
-eb list &&
-eb use udagram-api-env && eb setenv POSTGRES_USERNAME="postgres" POSTGRES_PASSWORD="password" POSTGRES_DB="postgres" POSTGRES_HOST="postgres.cznocuktxgrf.us-east-1.rds.amazonaws.com" PORT=8080 URL="http://localhost" JWT_SECRET="secret" &&
-eb deploy udagram-api-env
+eb setenv POSTGRES_USERNAME="postgres" POSTGRES_PASSWORD="password" POSTGRES_DB="postgres" POSTGRES_HOST="postgres.cznocuktxgrf.us-east-1.rds.amazonaws.com" PORT=8080 URL="http://localhost" JWT_SECRET="secret"
